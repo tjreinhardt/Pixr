2faf23ba1930092c4e33a443b08c20e54a434146
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-07-31 13:41:04 -0700

    minor fix

--- frontend/src/components/AddImage/index.js
+++ frontend/src/components/AddImage/index.js
@@ -127,7 +127,7 @@ const AddImage = () => {
               placeholder="Latitude"
               value={lat}
               onChange={updateLat}
-              required
+            // required
             />
 
             {/* {lat && (
